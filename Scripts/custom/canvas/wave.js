b88e124dfbf7159b482bc7931445bc901da3e88f
airbreak <954957945@qq.com>
2015-11-17 00:38:35 +0800

    add canvas wave

--- Scripts/custom/canvas/wave.js
+++ Scripts/custom/canvas/wave.js
@@ -1,12 +1,16 @@
-﻿$(function () {
-    
+﻿window.requestAnimationFrame = window.requestAnimationFrame
+    || window.mozRequestAnimationFrame
+    || window.webkitRequestAnimationFrame
+    || window.msRequestAnimationFrame;
+$(function () {
+
     function myCanvasWave() {
         this.canvas = document.getElementById('canvas');
         this.context = this.canvas.getContext('2d');
         this.canvas.width = this.canvas.parentNode.offsetWidth;
         this.canvas.height = this.canvas.parentNode.offsetHeight;
         this.step = 0;
-        window.requestAnimFrame(this.drawBlueWater);
+        window.requestAnimationFrame($.proxy(this, 'drawBlueWater'));
     }
 
     myCanvasWave.prototype = {
@@ -15,50 +19,43 @@
         drawBlueWater: function () {
             var height = this.canvas.height,
                 width = this.canvas.width;
-            this.context.clearRect(0, 0, height, width);
-            this.context.fillStyle = 'rgba(0,222,255,0.2)';
-            this.step++;
-
-            var angle = this.step * Math.PI / 180,
-                deltaHeight = Math.sin(angle) * 50;
-            this.context.beginPath();
-
-            this.context.moveTo(0, height / 2 + deltaHeight); //左上角
-            this.context.lineTo(0, height);//左下角
-            this.context.lineTo(width, height); //右下角
-            this.context.lineTo(width, height / 2 + deltaHeight); //右上角
+            this.context.clearRect(0, 0, width, height);
             
-            this.context.lineTo(0, height / 2 + deltaHeight); //左上角
+            
+            var lines = ["rgba(0,222,255, 0.2)", "rgba(157,192,249, 0.2)", "rgba(0,168,255, 0.2)"];
+            this.step++;
+            for (var i = 0; i < 3; i++) {
+                this.context.fillStyle = lines[i];
+                var angle = (this.step +i*45)* Math.PI / 180,
+                    deltaHeight = Math.sin(angle) * 50,
+                    deltaHeightRight = Math.cos(angle) * 50;
+                this.context.beginPath();
 
-            this.context.closePath();
-            this.context.fill();
-            window.requestAnimFrame(this.drawBlueWater);
+                this.context.moveTo(0, height / 2 + deltaHeight); //左上角
+
+                //this.context.lineTo(width, height / 2 + deltaHeightRight); //右上角
+
+                //曲线
+                var conPointX = width / 2,
+                    conPointY1 = height / 2 + deltaHeight,
+                    conPointY2 = height / 2 + deltaHeightRight;
+                this.context.bezierCurveTo(conPointX, conPointY1,
+                    conPointX, conPointY2,
+                    width, height / 2 + deltaHeightRight);
+
+                this.context.lineTo(width, height); //右下角
+
+                this.context.lineTo(0, height);//左下角
+
+                this.context.lineTo(0, height / 2 + deltaHeight); //左上角
+
+                this.context.closePath();
+                this.context.fill();
+            }
+            window.requestAnimationFrame($.proxy(this,'drawBlueWater'));
         },
-
-
-        requestAnimFrame: function (callback) {
-            var r1 = window.requestAnimationFrame,
-                r2 = window.webkitRequestAnimationFrame,
-                r3 = window.mozRequestAnimationFrame,
-                r4 = function (callback) {
-                    window.setTimeout(callback, 1000 / 60);
-                }
-            return r1 || r2 || r3 || r4;
-        },
-
-
-
     };
 
     new myCanvasWave();
 
 });
-
-window.requestAnimFrame = (function () {
-    return window.requestAnimationFrame ||
-     window.webkitRequestAnimationFrame ||
-        window.mozRequestAnimationFrame ||
-        function (callback) {
-            window.setTimeout(callback, 1000 / 60);
-        };
-})();
